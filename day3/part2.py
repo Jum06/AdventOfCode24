1b823b12f71deba5c7417587c83a2dd23e7cf140
emanuel <emanuel.kurz@it.htl-hl.ac.at>
2024-12-12 22:24:26 +0100

    began day3 part2

--- day3/part2.py
+++ day3/part2.py
@@ -4,22 +4,26 @@ file = open('data.txt')
 
 my_input = file.read()
 
-isEnabled = True
 
 def extract_factors(_input):
     pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
     matches = re.findall(pattern, _input)
     factors = [(int(a), int(b)) for a, b in matches]
-    print(factors)
+    print(matches)
     return factors
 
 def calc(_input):
-    factors = extract_factors(my_input)
+    factors = extract_factors(_input)
     result = 0
     for i in range(len(factors)):
         result += factors[i][0] * factors[i][1]
     return result
 
+def remove_dont_parts(_input):
+    _input += 'do()'
+    _input = re.sub(r'don\'t\(\).+?do\(\)', '', _input)
+    print(_input)
+    return _input
 
 
-print(calc(my_input))
+print(calc(remove_dont_parts(my_input)))
