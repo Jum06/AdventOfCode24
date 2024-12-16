966e48a4e310b098f059560877b6bfb0305ee111
emanuel <emanuel.kurz@it.htl-hl.ac.at>
2024-12-16 10:18:55 +0100

    finished day 5 part1

--- day5/part1.py
+++ day5/part1.py
@@ -1,3 +1,6 @@
+# D
+from operator import index
+
 with open("data.txt") as f:
     rules_str, updates_str = f.read().split("\n\n")
 
@@ -6,13 +9,31 @@ rules_str, updates_str = rules_str.splitlines(), updates_str.splitlines()
 rules = [list(map(int, item.split("|"))) for item in rules_str]
 updates = [list(map(int, item.split(","))) for item in updates_str]
 
+# Logic
+
+
 def check_update(update):
+    return not any(
+        rule[0] in update and rule[1] in update and not update.index(rule[0]) < update.index(rule[1])
+        for rule in rules
+    )
+    # return sum(
+    #     False
+    #     for rule in rules
+    #     if rule[0] in update and rule[1] in update
+    #     if not update.index(rule[0]) < update.index(rule[1])
+    # )
+    # for rule in rules:
+    #     if rule[0] in update and rule[1] in update:
+    #         if not update.index(rule[0]) < update.index(rule[1]):
+    #             return False
+    # return True
 
-    return False
+result = sum(
+    update[len(update) // 2]
+    for update in updates
+    if check_update(update)
+)
 
-def check_updates(updates):
-    for update in updates:
-        check_update(update)
-
-
-print(rules, updates)
+print(check_update(updates[3]))
+print(result)
