3b7b8833c25b26013879e730dbef6a93126a821b
Shorty <emanuel.kurz@stud.it.htl-hl.ac.at>
2024-12-19 11:46:45 +0100

    finished day7

---
+++ day7/part1.py
@@ -0,0 +1,41 @@
+with open("data.txt") as f:
+    _lines = f.readlines()
+
+_myInput = []
+
+for _line in _lines:
+    _myInput.append(list(map(int, _line.replace(":", "").strip().split(" "))))
+
+# Logic
+
+from itertools import product
+
+
+def check_line(line):
+    target = line[0]
+    numbers = line[1:]
+
+    operators = list(product(['+', '*', '||'], repeat=len(numbers) - 1))
+
+    for ops in operators:
+        result = numbers[0]
+        for i, op in enumerate(ops):
+            if op == '+':
+                result += numbers[i + 1]
+            elif op == '*':
+                result *= numbers[i + 1]
+
+        if result == target:
+            return True
+    return False
+
+
+def sum_true_lines(_input):
+    return sum(
+        line[0]
+        for line in _input
+        if check_line(line)
+    )
+
+print(_myInput)
+print(sum_true_lines(_myInput))
